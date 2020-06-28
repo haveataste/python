ce0bb54fa92d7c6096264af9ee2de78d889dd21d
athome <35514901+haveataste@users.noreply.github.com>
2020-06-28 20:14:47 +0800

    Add files via upload

---
+++ threading1.py
@@ -0,0 +1,16 @@
+import time
+from threading import Thread
+
+def m():
+    while True:
+        print(Thread.name, Thread.ident, 'Yes')
+        time.sleep(1)
+def f():
+    while True:
+        print(Thread.name, Thread.ident, 'Noo')
+        time.sleep(1)
+if __name__ == '__main__':
+    mt = Thread(target=m,args=())
+    ft = Thread(target=f,args=())
+    mt.start()
+    ft.start()
