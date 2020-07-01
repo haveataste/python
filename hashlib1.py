2cd78c0aa11271bd1c39ed3867373de11993a888
athome <35514901+haveataste@users.noreply.github.com>
2020-07-01 19:58:11 +0800

    Update hashlib1.py

--- hashlib1.py
+++ hashlib1.py
@@ -2,7 +2,7 @@ from hashlib import *
 import random
 
 s = 'abcd'
-f = random.choice([md5(), sha1(), sha256(), sha512()])
-f.update(bytes(s, encoding='utf-8'))
-result = f.hexdigest().upper()
-print(f, result, len(result), sep='\n')
+hf = random.choice([md5(), sha1(), sha256(), sha512()])
+hf.update(bytes(s, encoding='utf-8'))
+result = hf.hexdigest().upper()
+print(hf, result, len(result), sep='\n')
