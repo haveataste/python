2a94f82798e64d5be11fe4b7d863e8f2a711f19c
athome <35514901+haveataste@users.noreply.github.com>
2022-08-10 14:21:14 +0800

    Update multiprocessing_threading.py

--- multiprocessing_threading.py
+++ multiprocessing_threading.py
@@ -15,6 +15,7 @@ ft = Thread(target=f, args=())
 mt.start()
 ft.start()
 
+# CPython is Guido van Rossum’s reference version of the Python computing language. It’s most often called simply “Python”; speakers say “CPython” generally to distinguish it explicitly from other implementations.
 # GIL 是最流程的 CPython 解释器（平常称为 Python）中的一个技术术语，中文译为全局解释器锁，其本质上类似操作系统的 Mutex。GIL 的功能是：在 CPython 解释器中执行的每一个 Python 线程，都会先锁住自己，以阻止别的线程执行。
 # 计算密集型，用多进程
 # IO密集型，用多线程
